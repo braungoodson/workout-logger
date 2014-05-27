c95a638ad72d396caa8c5b3e1bda67999c7e0b7c
braun <braun@brauns-iMac.local>
2014-05-27 19:40:43 -0400

    added sets/create

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ app.config(['$routeProvider',Router]);
 function Router ($routeProvider) {
 	$routeProvider.when('/',{templateUrl:'views/index.html'});
 	$routeProvider.when('/workouts/create',{templateUrl:'views/workouts/create.html'});
+	$routeProvider.when('/sets/create',{templateUrl:'views/sets/create.html'});
 	$routeProvider.otherwise({redirectTo:'/'});
 }
