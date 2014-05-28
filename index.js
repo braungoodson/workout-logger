6655082e6296f45775a258e7d017c6dc2ce4f278
braun <braun@brauns-iMac.local>
2014-05-27 21:14:24 -0400

    stable

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var app = angular.module('app',['ngRoute']);
 
 app.config(['$routeProvider',Router]);
+app.controller('WorkoutsCreateController',['$scope','$http',WorkoutsCreateController]);
+app.controller('SetsCreateController',['$scope',SetsCreateController]);
 
 function Router ($routeProvider) {
 	$routeProvider.when('/',{templateUrl:'views/index.html'});
 	$routeProvider.when('/workouts/create',{templateUrl:'views/workouts/create.html'});
 	$routeProvider.when('/sets/create',{templateUrl:'views/sets/create.html'});
 	$routeProvider.otherwise({redirectTo:'/'});
+}
+
+function WorkoutsCreateController ($scope,$http) {
+	$scope.onSubmit = function () {
+		$http.post('/workouts',$scope.workout).success(function(){}).error(function(){});
+	}
+}
+
+function SetsCreateController ($scope) {
+	$scope.onSubmit = function () {
+		$http.post('/sets',$scope.set).success(function(){}).error(function(){});
+	}
 }
