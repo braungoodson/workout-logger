4296fb43130d203843a43a4c4bcd86db5ffaaec4
braun <braun@brauns-iMac.local>
2014-05-28 21:40:38 -0400

    added karma; added specs; added rest api;

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
   server = express(),
   port = process.env.PORT || 30000,
   staticRoot = __dirname;
 
+server.use(bodyParser());
 server.use('/', express.static(staticRoot));
 server.listen(port);
 console.log('http://localhost:'+port);
+
+server.post('/workouts',function(q,r){
+	var workout = q.body;
+	r.send(workout);
+});
---
+++ test/appSpec.js
@@ -0,0 +1,5 @@
+describe('app',function(){
+	it('should exist',function(){
+		expect(angular.module('app')).not.toBe(null);
+	});
+});
